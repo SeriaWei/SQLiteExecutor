fa61ab4644d64923e75fa82b2d3ff4f84d8cc10f
wayne <seriawei@outlook.com>
2020-04-11 21:40:26 +0800

    Support TRANSACTION block

--- SQLiteExecutor/Program.cs
+++ SQLiteExecutor/Program.cs
@@ -1,7 +1,10 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Data;
+using System.IO;
+using System.Text;
 
 namespace SQLiteExecutor
 {
@@ -25,22 +28,55 @@ namespace SQLiteExecutor
                 return 1;
             }
             Console.WriteLine("Script file: {0}", File);
-            string script = System.IO.File.ReadAllText(File);
+            List<string> scripts = ReadScripts(File);
             if (!string.IsNullOrWhiteSpace(Database))
             {
-                Execute(Database, script);
+                Execute(Database, scripts);
             }
             else
             {
                 foreach (var item in System.IO.Directory.GetFiles(System.IO.Directory.GetCurrentDirectory(), "*.sqlite"))
                 {
-                    Execute(item, script);
+                    Execute(item, scripts);
                 }
             }
             return 0;
         }
-
-        private void Execute(string dbFile, string script)
+        private List<string> ReadScripts(string scriptFile)
+        {
+            List<string> scripts = new List<string>();
+            using (FileStream fileStream = new FileStream(scriptFile, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    StringBuilder scriptBlock = new StringBuilder();
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.Equals("BEGIN TRANSACTION;", StringComparison.OrdinalIgnoreCase) ||
+                            line.Equals("COMMIT;", StringComparison.OrdinalIgnoreCase))
+                        {
+                            if (scriptBlock.Length > 0)
+                            {
+                                scripts.Add(scriptBlock.ToString());
+                                scriptBlock.Clear();
+                            }
+                        }
+                        else
+                        {
+                            scriptBlock.AppendLine(line);
+                        }
+                    }
+                    if (scriptBlock.Length > 0)
+                    {
+                        scripts.Add(scriptBlock.ToString());
+                        scriptBlock.Clear();
+                    }
+                }
+            }
+            return scripts;
+        }
+        private void Execute(string dbFile, List<string> scripts)
         {
             Console.WriteLine("Executing scripts to: {0}", dbFile);
             DateTime start = DateTime.Now;
@@ -54,12 +90,15 @@ namespace SQLiteExecutor
                 {
                     try
                     {
-                        using (var command = connection.CreateCommand())
+                        foreach (var script in scripts)
                         {
-                            command.Transaction = transaction;
-                            command.CommandTimeout = 0;
-                            command.CommandText = script;
-                            Console.WriteLine("{0} of rows affected Elapsed: {1}ms", command.ExecuteNonQuery(), (DateTime.Now - start).TotalMilliseconds);
+                            using (var command = connection.CreateCommand())
+                            {
+                                command.Transaction = transaction;
+                                command.CommandTimeout = 0;
+                                command.CommandText = script;
+                                Console.WriteLine("{0} of rows affected Elapsed: {1}ms", command.ExecuteNonQuery(), (DateTime.Now - start).TotalMilliseconds);
+                            }
                         }
                         transaction.Commit();
                     }
